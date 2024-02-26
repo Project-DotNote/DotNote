7fecc0bdd57473bad87dd6c10b0a9596daa753fa
Niko Nikolov <123800344+N1k0N1k0l0v@users.noreply.github.com>
2024-02-26 17:19:28 +0200

    The database that I have made (#8)
    * The database that I have made
    
    * Fix on the the database

---
+++ DotNote.Web/Models/Group.cs
@@ -0,0 +1,17 @@
+﻿namespace DotNote.Web.Models
+{
+    public class Group
+    {
+        public Group()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public User User { get; set; }
+    }
+}
