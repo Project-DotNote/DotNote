22365ea9bffbd74f34ac585d50c4b47d42b75854
JacksonJS12 <d.ivan0v.6100@gmail.com>
2024-03-10 23:33:46 +0200

    Added controller for calendar page.

---
+++ DotNote.Web/Controllers/CalendarController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNote.Web.Controllers
+{
+    public class CalendarController : Controller
+    {
+        public IActionResult Calendar()
+        {
+            return View();
+        }
+    }
+}
